28387c5fad0851adcc77e4af7da007b6ba5faf76
sanchez <dfitz360@gmail.com>
2022-09-22 19:49:23 +1000

    Got the attack timing working and particles

--- components/Attack.gd
+++ components/Attack.gd
@@ -2,17 +2,20 @@ extends Node
 
 signal on_attack(target)
 
-export (int) var COOLDOWN = 250
+export (int) var COOLDOWN = 1000
 export (int) var DAMAGE = 5
 
 onready var Timer := $Timer
 
 func _ready():
-	Timer.wait_time = COOLDOWN
+	Timer.wait_time = COOLDOWN / 1000.0
 	Timer.one_shot = true
+	
+func can_attack():
+	return Timer.time_left == 0
 
 func attack(target: Node2D, damage: Damage):
-	if Timer.is_stopped():
+	if can_attack():
 		# do the attack here
 		for x in target.get_children():
 			if x is HealthStatus:
--- enemies/Ghost.gd
+++ enemies/Ghost.gd
@@ -1,11 +1,12 @@
 extends Enemy
 
 var SEARCH_RANGE = 200
-var ATTACK_RANGE = 2 * Utils.TILE_SIZE * Utils.PIXEL_SIZE
+var ATTACK_RANGE = 30
 var ATTACK_AMOUNT = 3
 
 onready var Attack := $Attack
 onready var HealthStatus := $HealthStatus
+onready var Particles2D := $Particles2D
 
 var last_target = Vector2.ZERO
 var last_position = Vector2.ZERO
@@ -28,15 +29,15 @@ func get_new_random_location():
 	last_target = new_target
 	
 func attack(target):
-	# TODO: Animate the attack
-	pass
+	Particles2D.restart()
+	Particles2D.emitting = true
 	
 func process_attack(world: BaseWorld):
 	var players = world.get_players()
 	var closest_player = Utils.find_closest_node(get_position(), players)
 	if closest_player != null:
-		var dist = (closest_player.global_position - get_position()).length_squared()
-		if dist < (ATTACK_RANGE * ATTACK_RANGE):
+		var dist = (closest_player.global_position - get_position()).length()
+		if dist < ATTACK_RANGE:
 			var damage = Damage.new(ATTACK_AMOUNT)
 			Attack.attack(closest_player, damage)
 
@@ -44,7 +45,8 @@ func think(delta: float, world: BaseWorld):
 	process_attack(world)
 	
 	var target = get_target(world)
-	move_towards(target, delta)
+	if Attack.can_attack(): # if we have attacked and in our cooldown, I don't think we should move
+		move_towards(target, delta)
 	
 	# if we are close to our search then we want to get a new search target
 	var search_dist = (global_position - last_target).length_squared()
