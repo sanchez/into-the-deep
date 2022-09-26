3e07672ce23d17f1e887dc821c1a4bb613584c62
sanchez <dfitz360@gmail.com>
2022-09-26 20:58:00 +1000

    Fixed the frozen buff attribute

--- components/buffs/attributes/MovementModifierPerStack.gd
+++ components/buffs/attributes/MovementModifierPerStack.gd
@@ -3,6 +3,7 @@ class_name BAMovementModifierPerStack
 
 export (float) var ACCELERATION_MODIFIER = 0.8
 export (float) var MAX_SPEED_MODIFIER = 0.5
+export (int) var MAX_STACKS = 100
 
 var been_applied = false
 var slowed_accel_amount = 0
@@ -12,20 +13,20 @@ func calculate_amount(modifier, current_amount):
 	var total = modifier * current_amount
 	if total > current_amount:
 		return current_amount
-	return modifier
+	return total
 
 func remove_buff(health):
 	if been_applied:
-		health.owner.ACCELERATION += slowed_accel_amount
+		#health.owner.ACCELERATION += slowed_accel_amount
 		health.owner.MAX_SPEED += slowed_max_speed_amount
 		been_applied = false
 		
 func apply_buff(buff, health):
-	slowed_accel_amount = calculate_amount(ACCELERATION_MODIFIER * buff.stack, health.owner.ACCELERATION)
-	health.owner.ACCELERATION -= slowed_accel_amount
+	var buff_percent = clamp(buff.stack * 1.0 / MAX_STACKS, 0.0, 1.0)
+	#slowed_accel_amount = calculate_amount(ACCELERATION_MODIFIER * buff.stack, health.owner.ACCELERATION)
+	#health.owner.ACCELERATION -= slowed_accel_amount
 	
-	print(health.owner.MAX_SPEED * buff.stack * MAX_SPEED_MODIFIER)
-	slowed_max_speed_amount = calculate_amount(MAX_SPEED_MODIFIER * buff.stack, health.owner.MAX_SPEED)
+	slowed_max_speed_amount = calculate_amount(MAX_SPEED_MODIFIER * buff_percent, health.owner.MAX_SPEED)
 	health.owner.MAX_SPEED -= slowed_max_speed_amount
 	
 	been_applied = true
