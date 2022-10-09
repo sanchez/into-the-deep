0479ca46fb0705669389949e6dc03895b6f5fdeb
sanchez <dfitz360@gmail.com>
2022-10-09 14:46:22 +1000

    got the inventory functionality working good enough for now

--- ui/InvItem/InvItem.gd
+++ ui/InvItem/InvItem.gd
@@ -1,7 +1,15 @@
 extends Panel
 
+const DefaultInvItemStyle := preload("res://ui/InvItem/DefaultInvItemStyle.tres")
+const SelectedInvItemStyle := preload("res://ui/InvItem/SelectedInvItemStyle.tres")
+
+signal on_item_selected(slot)
+
 export (Array) var INVENTORY
 export (int) var SLOT
+
+export (bool) var ONLY_WEAPON = false
+export (bool) var ONLY_ARTIFACT = false
 
 onready var TextureRectNode := $TextureRect
 
@@ -9,6 +17,15 @@ func get_item():
 	#if INVENTORY != null and INVENTORY.size() < SLOT:
 	return INVENTORY[SLOT]
 	#return null
+	
+func set_selected(selected):
+	if selected:
+		add_stylebox_override("panel", SelectedInvItemStyle)
+	else:
+		add_stylebox_override("panel", DefaultInvItemStyle)
+		
+func _ready():
+	set_selected(false)
 
 func _process(_delta):
 	var item = get_item()
@@ -16,3 +33,9 @@ func _process(_delta):
 		TextureRectNode.texture = item.ICON
 	else:
 		TextureRectNode.texture = null
+
+func _on_InvItem_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == BUTTON_LEFT:
+			set_selected(true)
+			emit_signal("on_item_selected")
--- ui/InventoryPicker/InventoryPicker.gd
+++ ui/InventoryPicker/InventoryPicker.gd
@@ -10,6 +10,41 @@ onready var StoredNode := $Control/ColorRect2/MarginContainer/VBoxContainer/Stor
 onready var WeaponNode := $Control/ColorRect2/MarginContainer/VBoxContainer/TopRow/Weapon
 
 var show_inventory = false
+var selected_slot = null
+
+func bind_inv_slot_handlers(slot):
+	slot.connect("on_item_selected", self, "handle_item_selected", [slot])
+	
+func can_input_item(slot, item) -> bool:
+	if not is_instance_valid(item):
+		return true
+		
+	if slot.ONLY_WEAPON:
+		if not item is Weapon:
+			return false
+			
+	if slot.ONLY_ARTIFACT:
+		if not item is Artifact:
+			return false
+			
+	return true
+	
+func handle_item_selected(slot):
+	if is_instance_valid(selected_slot):
+		# swap the slots
+		
+		var a = slot.get_item()
+		var b = selected_slot.get_item()
+		
+		if can_input_item(slot, b) and can_input_item(selected_slot, a):
+			slot.INVENTORY[slot.SLOT] = b
+			selected_slot.INVENTORY[selected_slot.SLOT] = a
+		
+		slot.set_selected(false)
+		selected_slot.set_selected(false)
+		selected_slot = null
+	else:
+		selected_slot = slot
 
 func get_inventory_player():
 	var inventory = get_node(INVENTORY_PATH)
@@ -49,11 +84,14 @@ func _ready():
 	
 	WeaponNode.INVENTORY = inventory.EQUIPPED_WEAPON
 	WeaponNode.SLOT = 0
+	bind_inv_slot_handlers(WeaponNode)
 	
 	for x in range(0, artifact_slots):
 		var invItem = InvItem.instance()
 		invItem.INVENTORY = inventory.EQUIPPED_ARTIFACTS
 		invItem.SLOT = x
+		invItem.ONLY_ARTIFACT = true
+		bind_inv_slot_handlers(invItem)
 		TopRowNode.add_child(invItem)
 	
 	var spare_slots = inventory.MAX_SPARE_SLOTS
@@ -61,10 +99,39 @@ func _ready():
 		var invItem = InvItem.instance()
 		invItem.INVENTORY = inventory.spare_slots
 		invItem.SLOT = x
+		bind_inv_slot_handlers(invItem)
 		StoredNode.add_child(invItem)
+		
+func close():
+	show_inventory = false
+	visible = show_inventory
+	if is_instance_valid(selected_slot):
+		selected_slot.set_selected(false)
+	selected_slot = null
 
 func _input(event):
 	if event.is_action_pressed("inventory"):
 		show_inventory = not show_inventory
 		#owner.get_tree().paused = show_inventory
 		visible = show_inventory
+		if not show_inventory:
+			if is_instance_valid(selected_slot):
+				selected_slot.set_selected(false)
+			selected_slot = null
+
+#func _process(_delta):
+	#if is_instance_valid(selected_slot):
+	#	var item = selected_slot.get_item()
+	#	if is_instance_valid(item):
+	#		HoverItemNode.texture = item.ICON
+	#	else:
+	#		HoverItemNode.texture = null
+	#	HoverItemNode.visible = true
+	#else:
+	#	HoverItemNode.visible = false
+
+
+func _on_ColorRect_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == BUTTON_LEFT:
+			close()
